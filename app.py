6871cb0903c8063d008bdd71aa9e2ef24d8b1a79
Hank Liu <jliu73@nait.ca>
2022-06-10 12:54:16 -0600

    fixed startup

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from .extensions import db
+from .routes.submit_logs import submitLogs
+from .routes.retrieve_logs import retrieveLogs
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logs.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+
+    app.register_blueprint(submitLogs)
+    app.register_blueprint(retrieveLogs)
+
+    return app
+
+def __main__():
+    app = create_app()
+    app.run(debug=True)
